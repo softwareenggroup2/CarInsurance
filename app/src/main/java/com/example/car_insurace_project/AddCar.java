3a7e7a5042ae3cfd1e95050660a8a79b83a90e80
Bowins <Bowins@live.co.uk>
2019-05-08 23:12:14 +0100

    Revert "Update content___main_login.xml"
    This reverts commit 5e8e22d68bd4e5d9e5f7a90dcc01c08ff6ed5549.

--- app/src/main/java/com/example/car_insurace_project/AddCar.java
+++ app/src/main/java/com/example/car_insurace_project/AddCar.java
@@ -4,24 +4,22 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class AddCar extends Fragment {
+public class AddCar extends FragmentActivity {
 
     @Nullable
-    @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.add_car,null);
     }
 
-    @Override
+
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-
-
+        //super.onViewCreated(view, savedInstanceState);
 
     }
 }
--- app/src/main/java/com/example/car_insurace_project/RandomIncident.java
+++ app/src/main/java/com/example/car_insurace_project/RandomIncident.java
@@ -4,22 +4,23 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class RandomIncident extends Fragment {
+public class RandomIncident extends FragmentActivity {
 
 
     @Nullable
-    @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.random_incident,null);
     }
 
-    @Override
+
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        //super.onViewCreated(view, savedInstanceState);
 
 
     }
